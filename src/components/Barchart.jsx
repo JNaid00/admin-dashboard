1df20799be40319c9653730f750f6de8c257f46c
Jesse Naidoo <jessenaidoo24@gmail.com>
2022-11-27 22:19:08 +0200

    Updated bar Chart

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -2,7 +2,7 @@ import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens, useMode} from "../theme";
 import { mockBarData as data } from "../data/mockData";
-const BarChart = ({ isDashboard = false }) => {
+const BarChart = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
